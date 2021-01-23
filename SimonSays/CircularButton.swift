d6c814f12234df3a6bbd0a1b7a4c429de0856d0a
KFischer(M13) <klaus.fischer@eloware.com>
2021-01-23 17:13:17 +0100

    Initial commit

---
+++ SimonSays/CircularButton.swift
@@ -0,0 +1,24 @@
+//
+//  CircularButton.swift
+//  SimonSays
+//
+//  Created by Klaus Fischer on 23.01.21.
+//
+
+import UIKit
+
+class CircularButton: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = frame.size.height * 0.4
+        layer.masksToBounds = true
+        
+    }
+
+    override var isHighlighted: Bool{
+        didSet{
+            alpha = isHighlighted ? 1.0 : 0.5
+        }
+    }
+    
+}
--- SimonSays/ViewController.swift
+++ SimonSays/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var scoreLabels: [UILabel]!
+    @IBOutlet var playerLabels: [UILabel]!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet var colorButtons: [CircularButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        colorButtons = colorButtons.sorted(){            $0.tag < $1.tag        }
+        playerLabels = playerLabels.sorted(){            $0.tag < $1.tag        }
+        scoreLabels = scoreLabels.sorted(){            $0.tag < $1.tag        }
+        createNewGame()
     }
 
-
+    func createNewGame(){
+        actionButton.setTitle("Start Game", for: .normal)
+        for button in colorButtons{
+            button.alpha = 0.5
+        }
+    }
+        
+    @IBAction func colorButtonHandle(_ sender: CircularButton) {
+        print("Button \(sender.tag) tapped")
+        
+    }
+    
+    @IBAction func actionButtonHandler(_ sender: UIButton) {
+        print("Action Button")
+    }
 }
 
